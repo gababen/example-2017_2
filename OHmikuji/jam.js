b0cf460bba0a7be27d9af50f1b4c6dd4312bfdf0
Nana Harada <16121039@edu.cc.saga-u.ac.jp>
2017-06-14 11:15:35 +0900

    変更

--- OHmikuji/jam.js
+++ OHmikuji/jam.js
@@ -5,12 +5,18 @@
     
     btn.addEventListener('click',function(){
       var n= Math.random();
-        if(n<0.05){
+        if(n<0.1){
             this.textContent='大吉';
-        }else if(n<0.2){
+        }else if(n<0.3){
             this.textContent='中吉';
-        }else{
-            this.textContent='凶';
+        }else if(n<0.5){
+            this.textContent='末吉';
+        }else if(n<0.7){
+            this.textContent='小吉';
+        }else if(n<0.95){
+            this.textContent='吉';
+        }else {
+            this.textContent='凶'
         }
         //  var results=['大吉','大吉','大吉','中吉','凶','末吉'];
         //var n =Math.floor(Math.random()* results.length);
